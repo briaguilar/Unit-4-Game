22b007af9aa97fb1c7d4f7a6b7e9d0b1abfb8ea1
Bri <breezy.aguilar@gmail.com>
2019-06-11 11:25:34 -0500

    Folders and files have all been created.

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//5 Random generators: 1 for random.number, 4 crystals
+
+// Computer picks random number (19-120)
+// Computer picks random number for crystals (1-12)
+// After pick # for crystals, give it a variable
+// User clicks on crystal,
+    // Adds random # to score counter
+    // If score === random-number:
+        // Win++
+        // Game restarts
+    // If score > random.number:
+        // Losses++
+        // Game restarts
